b27c27c2cc20c6cd548257483942d41393d2a581
Dimitri Temkin <dimitri.temkin@gmail.com>
2017-03-15 09:20:16 +1100

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,10 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/approveme');
+
+// MONGODB_URI: mongodb://heroku_1kl2jhx6:pl5ufa2ofc37177skf0rsdm686@ds131480.mlab.com:31480/heroku_1kl2jhx6
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/approveme');
+
+
 
 module.exports = {mongoose};
